35fcb539a14f60974dbfcc133ba7c42e19a51e45
JohnJScott <johnjscott@eternaldevelopments.com>
2022-08-21 20:26:03 -0400

    Delete ResilientP4 directory

--- ResilientP4/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright 2015 Eternal Developments LLC. All Rights Reserved.
-
-using System.Reflection;
-
-[assembly: AssemblyTitle( "ResilientP4" )]
-[assembly: AssemblyDescription( "A utility to use Perforce over unreliable connections." )]
-[assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyProduct( "EternalUtilities" )]
