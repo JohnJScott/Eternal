899608503ae4d915ad2f4b0053d1d44aa71aec88
JohnJScott <johnjscott@eternaldevelopments.com>
2022-08-21 20:25:54 -0400

    Delete PdbInterrogator directory

--- PdbInterrogator/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright 2015 Eternal Developments LLC. All Rights Reserved.
-
-using System.Reflection;
-
-[assembly: AssemblyTitle( "PdbInterrogator" )]
-[assembly: AssemblyDescription( "A utility to analyse PDB files." )]
-[assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyProduct( "EternalUtilities" )]
