3e457f7bbabb0b6d9862d7b91b365f9e642d3e66
JohnJScott <johnjscott@eternaldevelopments.com>
2022-08-21 20:26:53 -0400

    Delete UTF16MustDie directory

--- UTF16MustDie/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-// Copyright 2021 Eternal Developments LLC. All Rights Reserved.
-
-using System.Reflection;
-
-[assembly: AssemblyTitle( "UTF16MustDie" )]
-[assembly: AssemblyDescription( "A utility to hunt down, fix, and then kill UTF16 files" )]
-[assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyProduct( "UTF16MustDie" )]
